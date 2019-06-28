1252493d55ac2799ee9853e3bfc5e33c2c4c6614
Laura Felce <crepu83@yahoo.it>
2019-06-28 09:01:13 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,7 @@ class SongsController < ApplicationController
     else
       @songs = Song.all
       redirect_to @songs
-      flash[:alert] = "Artist not found."
+      [:alert] = "Artist not found."
     end
   end
 
