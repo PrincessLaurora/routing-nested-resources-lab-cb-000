30dccef80e011995d5267840fd0e354d91cc4aa6
Laura Felce <crepu83@yahoo.it>
2019-06-28 10:37:54 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,12 +15,17 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])
+
     if params[:artist_id]
       @artist = Artist.find(params[:artist_id])
       if @artist.nil?
         redirect_to artist_songs_path(@artist), alert: "Song not found."
-
+      else
+        @song = @artist.song
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
