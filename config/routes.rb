78b6f5f650c0c15bc02cbb7d90e49cdcfaacd5ee
Laura Felce <crepu83@yahoo.it>
2019-06-28 10:46:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :artists do
       # nested resource for posts
-      resources :songs, only: [:index, v]
+      resources :songs, only: [:index, :show]
     end
 
     resources :songs
