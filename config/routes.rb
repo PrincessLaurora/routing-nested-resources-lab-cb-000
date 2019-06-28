b50109fcfbf1ef19c5dc2317d27c570ea9d10815
Laura Felce <crepu83@yahoo.it>
2019-06-28 08:27:43 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
       # nested resource for posts
       resources :songs, only: [:show, :index]
     end
-   
+
     resources :songs, only: [:index, :show, :new, :create, :edit, :update]
-   
+
     root 'songs#index'
 end
