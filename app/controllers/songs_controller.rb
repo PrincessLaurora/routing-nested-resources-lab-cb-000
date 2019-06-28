abaec3406e1db90b29d354d542fcb3e9b7b2b8e1
Laura Felce <crepu83@yahoo.it>
2019-06-28 09:09:49 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,9 @@ class SongsController < ApplicationController
 
   def index
     if params[:artist_id]
+      @artist = Artist.find(params[:artist_id])
       @songs = Artist.find(params[:artist_id]).songs
+      if artist
     else
       @songs = Song.all
       redirect_to @songs
